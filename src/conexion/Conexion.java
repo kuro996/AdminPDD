41b88079562bb286c208323cb2041ad70cae4faa
libanez <libanez@WKS101.softoffice.com.ar>
2019-01-07 12:10:51 -0300

    asd

---
+++ src/conexion/Conexion.java
@@ -0,0 +1,60 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion
+{
+	public static Conexion instancia;
+	private Connection connection;
+
+	private Conexion()
+	{
+		try
+		{
+			String path = "Configuracion\\base.properties";
+			String ip = Propiedades.getPropertyValue("baseIP", path);
+			String puerto = Propiedades.getPropertyValue("basePuerto", path);
+			String schema = Propiedades.getPropertyValue("baseSchema", path);
+			String url = "jdbc:mysql://" + ip + ":" + puerto + "/" + schema + "";
+			String username = Propiedades.getPropertyValue("baseUsuario", path);
+			String password = Propiedades.getPropertyValue("baseContrasenia", path);
+			this.connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Conexion exitosa");
+		}
+		catch (Exception e)
+		{
+			System.out.println("Conexion fallida");
+			throw new RuntimeException("Conexion fallida");
+		}
+	}
+
+	public static Conexion getConexion()
+	{
+		if (instancia == null)
+		{
+			instancia = new Conexion();
+		}
+		return instancia;
+	}
+
+	public Connection getSQLConexion()
+	{
+		return this.connection;
+	}
+
+	public void cerrarConexion()
+	{
+		try
+		{
+			this.connection.close();
+			System.out.println("Conexion cerrada");
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		instancia = null;
+	}
+}
---
+++ src/conexion/Propiedades.java
@@ -0,0 +1,51 @@
+package conexion;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
+public class Propiedades
+{
+	public static String getPropertyValue(String propertyKey, String path)
+	{
+		Properties p = new Properties();
+		try
+		{
+			p.load(new FileReader(path));
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.println(e.getMessage());
+			return "";
+		}
+		catch (IOException e)
+		{
+			System.out.println(e.getMessage());
+			return "";
+		}
+		return p.getProperty(propertyKey);
+	}
+
+	public static void setPropertyValue(String propertyKey, String path, String valor)
+	{
+		Properties p = new Properties();
+		try
+		{
+			p.load(new FileReader(path));
+			OutputStream oute = new FileOutputStream(path);
+			p.setProperty(propertyKey, valor);
+			p.store(oute, "");
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
