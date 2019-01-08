d5936c30149a2874ca64879d3d4c1a1443ec00e3
libanez <libanez@WKS101.softoffice.com.ar>
2019-01-08 11:29:14 -0300

    dfs

--- src/Pantallas/frmLogin.java
+++ src/Pantallas/frmLogin.java
@@ -1,5 +1,21 @@
 package Pantallas;
 
-public class frmLogin {
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class frmLogin extends JFrame {
+	
+	public frmLogin() {
+		try {
+			UIManager.setLookAndFeel();
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	   
+		
+	}
 
 }
