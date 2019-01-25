00692497268b2ed9856fbf192f3bf69a9b010863
Leandro Ibañez <Leandro Ibaez@KURO-PC>
2019-01-24 23:47:49 -0300

    sp

--- Querys/Creacion SPs.sql
+++ Querys/Creacion SPs.sql
@@ -35,7 +35,7 @@ DELIMITER ;
 DROP PROCEDURE IF EXISTS get_permisos;
 
 DELIMITER //
-CREATE PROCEDURE get_permisos(codigo VARCHAR(255), usuario INT,equ INT)
+CREATE PROCEDURE get_permisos(codigo VARCHAR(255), usuario INT,equ INT,conEqu INT)
 BEGIN
 
 DROP TABLE IF EXISTS Funciones;
@@ -48,7 +48,7 @@ SET @count=0;
 
 INSERT INTO Funciones VALUES(@count,@fun_id,codigo,@pantalla);
 
-IF(@tipo_usu='A') THEN
+IF(@tipo_usu='A' AND conEqu<>0) THEN
 	WHILE @count <= (SELECT MAX(id) FROM Funciones) DO
 		
 		INSERT INTO Funciones 
@@ -72,7 +72,7 @@ ELSE
 			fun_nombre
 		FROM wf_funciones 
 			INNER JOIN wf_asignaciones ON asi_fun=fun_id 
-			INNER JOIN wf_usuarios ON usu_id=asi_usu OR asi_equ IN (SELECT equ_id FROM wf_equipos INNER JOIN wf_miembros ON mie_equ=equ_id WHERE mie_usu=usu_id AND mie_baja=0 AND equ_baja=0)
+			INNER JOIN wf_usuarios ON usu_id=asi_usu OR asi_equ IN (SELECT equ_id FROM wf_equipos INNER JOIN wf_miembros ON mie_equ=equ_id WHERE mie_usu=usu_id AND mie_baja=0 AND equ_baja=0 AND conEqu<>0)
 		WHERE fun_padre=(SELECT fun FROM Funciones WHERE id=@count) AND usu_id=usuario AND asi_baja=0 AND usu_baja=0 AND fun_baja=0;
 		
 		SET @count=@count+1;
