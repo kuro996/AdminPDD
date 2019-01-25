056e2cbc0af61d568b662193aca4de5674fe9056
libanez <libanez@WKS101.softoffice.com.ar>
2019-01-25 17:11:25 -0300

    sda

--- src/Pantallas/frmEquipos.java
+++ src/Pantallas/frmEquipos.java
@@ -2,8 +2,34 @@ package Pantallas;
 
 import javax.swing.JDialog;
 
+import java.awt.Dimension;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+
 public class frmEquipos extends JDialog{
 	public frmEquipos() {
+		setMinimumSize(new Dimension(400, 500));
+		setTitle("Equipos");
+		setModal(true);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(166)
+					.addComponent(lblNewLabel)
+					.addContainerGap(222, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(120)
+					.addComponent(lblNewLabel)
+					.addContainerGap(127, Short.MAX_VALUE))
+		);
+		getContentPane().setLayout(groupLayout);
 	}
-
 }
--- src/Pantallas/frmUsuarios.java
+++ src/Pantallas/frmUsuarios.java
@@ -1,11 +1,19 @@
 package Pantallas;
 
 import javax.swing.JDialog;
+
+import java.awt.Dimension;
+
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 
 public class frmUsuarios extends JDialog{
 	public frmUsuarios() {
+		
+		setMinimumSize(new Dimension(400, 500));
+		setTitle("Usuarios");
+		setModal(true);
+		
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
